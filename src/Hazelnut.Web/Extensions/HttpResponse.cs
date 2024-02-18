4ae1fba24d36a373b8ac398f83ef8941c1749ac5
Waqas Aziz <waqasazizuae@hotmail.com>
2024-02-18 19:01:03 +0000

    rearrange projects

--- src/Hazelnut.Web/Extensions/HttpResponse.cs
+++ src/Hazelnut.Web/Extensions/HttpResponse.cs
@@ -2,8 +2,8 @@
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Hazelnut.Entities;
+using Microsoft.AspNetCore.Http;
 
 namespace Hazelnut
 {
