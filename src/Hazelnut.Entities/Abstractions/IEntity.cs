cb77a196e27db9f681f7d085b28ab1833776abc5
Muhammad Atir Naveed <atirnaveed@gmail.com>
2024-02-18 20:00:07 +0000

    update

--- src/Hazelnut.Entities/Abstractions/IEntity.cs
+++ src/Hazelnut.Entities/Abstractions/IEntity.cs
@@ -7,23 +7,13 @@ namespace Hazelnut.Entities
     /// <summary>
     /// Represents an M# Entity.
     /// </summary>
-    public interface IEntity : IComparable
+    public interface IEntity
     {
-        /// <summary>
-        /// Determines whether this object has just been instantiated as a new object, or represent an already persisted instance.
-        /// </summary>
-        bool IsNew { get; }
 
         /// <summary>
         /// Gets the id of this entity.
         /// </summary>        
         object GetId();
-
-        /// <summary>
-        /// Creates a new object that is a copy of the current instance.
-        /// </summary>
-        /// <returns>A new object that is a copy of this instance.</returns>
-        IEntity Clone();
 
     }
 
@@ -35,7 +25,7 @@ namespace Hazelnut.Entities
         /// <summary>
         /// Gets the ID.
         /// </summary>
-        T ID { get; set; }
+        T Id { get; set; }
     }
 
     internal interface IOriginalIdHolder { void SetOriginalId(); }
