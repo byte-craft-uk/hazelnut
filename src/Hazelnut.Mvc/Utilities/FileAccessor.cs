8f8410f1c57792e62c813576c50a329a4398a07e
Muhammad Atir Naveed <atirnaveed@gmail.com>
2024-02-27 01:35:45 +0000

    Add file accessor

---
+++ src/Hazelnut.Mvc/Utilities/FileAccessor.cs
@@ -0,0 +1,52 @@
+﻿using Hazelnut.Entities;
+using System.Reflection;
+using System.Security.Principal;
+
+namespace Hazelnut.Mvc
+{
+    public class FileAccessor
+    {
+        protected readonly Type Type;
+        protected readonly PropertyInfo PropertyInfo;
+        protected string Id;
+        protected readonly IPrincipal CurrentUser;
+
+        public IEntity Instance { get; private set; }
+
+        public Blob Blob { get; private set; }
+
+        public string SecurityErrors { get; private set; }
+
+        /// <summary>
+        /// Use create method to instantiate the class.
+        /// </summary>
+        public FileAccessor(Type type, PropertyInfo propertyInfo, string id, IPrincipal user)
+        {
+            Type = type;
+            PropertyInfo = propertyInfo;
+            Id = id;
+            CurrentUser = user;
+        }
+
+        public async Task LoadBlob()
+        {
+            await FindRequestedObject();
+        }
+
+        async Task FindRequestedObject()
+        {
+            foreach (var key in new[] { ".", "/" })
+                if (Id.Contains(key)) Id = Id.Substring(0, Id.IndexOf(key));
+
+            Instance = Type as IEntity;
+
+            if (Instance == null) throw new Exception($"Invalid {Type.FullName} ID specified: '{Id}'");
+
+            Blob = PropertyInfo?.GetValue(Instance) as Blob;
+            if (Blob == null)
+                throw new Exception("Failed to find a Blob property named '" + PropertyInfo.Name
+                + "' on " + Instance.GetType().FullName + ".");
+        }
+
+    }
+}
---
+++ src/Hazelnut.Mvc/Utilities/FileAccessorFactory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Security.Principal;
+using Hazelnut.Entities;
+
+namespace Hazelnut.Mvc
+{
+    public class FileAccessorFactory : IFileAccessorFactory
+    {
+
+        public FileAccessorFactory()
+        {
+        }
+
+        public async Task<FileAccessor> Create(string path, IPrincipal currentUser)
+        {
+            var temp = GetTypeAndProperty(path);
+
+            var result = new FileAccessor(temp.Type, temp.PropertyInfo, temp.Id, currentUser);
+
+            await result.LoadBlob();
+
+            return result;
+        }
+
+        protected virtual (Type Type, PropertyInfo PropertyInfo, string Id) GetTypeAndProperty(string path)
+        {
+            var pathParts = path.Split('/');
+
+            if (pathParts.Length < 2)
+                throw new Exception($"Invalid path specified: '{path}'");
+
+            var typeName = pathParts[0].Split('.')[0];
+
+
+            var type = AppDomain.CurrentDomain.FindImplementers(typeof(IEntity), ignoreDrivedClasses: false)
+                        .SingleOrDefault(x => x.Name == typeName);
+
+            if (type == null) throw new Exception($"Invalid type name specified: '{typeName}'");
+
+            var property = pathParts[0].Split('.')[1];
+
+            var propertyInfo = type.GetProperty(property);
+            if (propertyInfo == null)
+                throw new Exception($"Could not find the property '{property}' on the type '{type.FullName}'.");
+
+            return (type, propertyInfo, pathParts[1]);
+        }
+    }
+}
